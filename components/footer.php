830d43ea213bcff8da3bbf0fc9a24acdfdbbe56c
ThankGod Okoro <ebuzzadvert@gmail.com>
2024-01-28 01:39:44 +0100

    first commit

---
+++ components/footer.php
@@ -0,0 +1,103 @@
+    <footer class="footer pt-4 pb-2 pb-md-5 pt-sm-5">
+        <div class="container-fluid pb-1 pb-md-0 px-md-5">
+            <div class="bg-dark rounded-5 position-relative overflow-hidden w-100 py-5 px-3 px-sm-4 px-xl-5 mx-auto" style="max-width: 1660px;" data-bs-theme="dark">
+                <div class="position-absolute top-50 start-50 translate-middle" style="width: 1664px;">
+                    <img src="assets/img/footer-wave.png" alt="Wave">
+                </div>
+                <div class="container position-relative z-2 pt-md-3 pt-lg-4 pt-xl-5 pb-2">
+                    <div class="row pb-2">
+                        <div class="col-lg-4 col-xxl-3 pb-2 pb-lg-0 mb-4 mb-lg-0">
+                            <div class="navbar-brand text-light py-0 me-0 pb-1 mb-3">
+                                <img src="assets/img/logoLight.svg" width="250" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-lg-2 offset-lg-1 offset-xl-2 offset-xxl-3 mb-4 mb-sm-0">
+                            <h6 class="fw-bold">Quick Link</h6>
+                            <ul class="nav flex-column fs-sm">
+                                <li class="nav-item">
+                                    <a class="nav-link fw-normal px-0 py-1" href="./">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-normal px-0 py-1" href="about">About us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-normal px-0 py-1" href="driver">Drive for kewiss</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-normal px-0 py-1" href="partnership">Partnership</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-lg-2 mb-4 mb-sm-0">
+                            <h6 class="fw-bold">Support</h6>
+                            <ul class="nav flex-column fs-sm">
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="help">Help center</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="terms">Terms of service</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="faq">FAQ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="privacy">Privacy policy</a>
+                            </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-lg-3 col-xl-2">
+                            <h6 class="fw-bold">Contact us</h6>
+                            <ul class="nav flex-column fs-sm">
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="mailto:hello@kewiss.com">hello@kewiss.com</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="tel:+15262200459">+234 907 060 4413</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fw-normal px-0 py-1" href="tel:+15262200444">+234 803 950 1607</a>
+                            </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="d-sm-flex align-items-center justify-content-between pt-4 pt-md-5 mt-2 mt-md-0 mt-lg-2 mt-xl-4">
+                        <div class="d-flex justify-content-center order-sm-2 me-md-n2">
+                            <a class="btn btn-icon btn-sm btn-secondary btn-instagram rounded-circle mx-2" target="_blank" rel="noopener" href="https://www.instagram.com/kewissride" aria-label="Instagram">
+                            <i class="ai-instagram"></i>
+                            </a>
+                            <a class="btn btn-icon btn-sm btn-secondary btn-facebook rounded-circle mx-2" target="_blank" rel="noopener" href="https://www.facebook.com/kewissride" aria-label="Facebook">
+                            <i class="ai-facebook"></i>
+                            </a>
+                            <a class="btn btn-icon btn-sm btn-secondary btn-tiktok rounded-circle mx-2" target="_blank" rel="noopener" href="https://www.instagram.com/kewissride" aria-label="Tiktok">
+                            <i class="ai-tiktok"></i>
+                            </a>
+                            <a class="btn btn-icon btn-sm btn-secondary btn-x rounded-circle mx-2" target="_blank" rel="noopener" href="https://www.x.com/kewissride" aria-label="X">
+                            <i class="ai-x"></i>
+                            </a>
+                        </div>
+                        <p class="nav fs-sm order-sm-1 text-center text-sm-start pt-4 pt-sm-0 mb-0 me-4">
+                            <span class="text-body-secondary">&copy; <script>document.write(new Date().getFullYear());</script> All rights reserved. Built by</span>
+                            <a class="nav-link fw-normal p-0 ms-1" href="https://webify.com.ng" target="_blank" rel="noopener">Webify</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Back to top button -->
+    <a class="btn-scroll-top" href="#top" data-scroll aria-label="Scroll back to top">
+      <svg viewBox="0 0 40 40" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="19" fill="none" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10"></circle>
+      </svg>
+      <i class="ai-arrow-up"></i>
+    </a>
+
+    <script src="assets/vendor/parallax-js/dist/parallax.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/aos/dist/aos.js"></script>
+
+    <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
---
+++ components/navbar.php
@@ -0,0 +1,49 @@
+    <header class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+
+            <!-- Navbar brand (Logo) -->
+            <a class="navbar-brand pe-sm-3" href="./">
+                <span class="text-primary flex-shrink-0 me-2">
+                <img src="assets/img/logoDark.svg" alt="logo" width="100">
+            </a>
+
+            <a class="btn btn-outline-primary me-2 fs-sm order-lg-3 d-none d-sm-inline-flex" href="download">
+                Partnership
+            </a>
+
+            <a class="btn btn-dark fs-sm order-lg-3 d-none d-sm-inline-flex" href="download">
+                Download
+            </a>
+
+            <!-- Mobile menu toggler (Hamburger) -->
+            <button class="navbar-toggler ms-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Navbar collapse (Main navigation) -->
+            <nav class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav navbar-nav-scroll mx-auto" style="--ar-scroll-height: 520px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="components/typography.html">How it works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="docs/getting-started.html">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="docs/getting-started.html">Safety</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="docs/getting-started.html">Help</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="faq">FAQ</a>
+                    </li>
+                </ul>
+                <div class="d-sm-none p-3 mt-n3">
+                    <a class="btn btn-dark w-100 mb-1" href="download">
+                        Download
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </header>
